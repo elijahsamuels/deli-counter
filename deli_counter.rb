6cc98806765ae0a9b9a1caf1eb5acf4666a5539a
elijahsamuels <elijahsamuels@gmail.com>
2020-11-11 20:48:55 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(peopleInLine)
+    if peopleInLine <= 0
+        puts "The line is currently empty."
+    else
+        puts "take a number!"
+end
+line(peopleInLine)
