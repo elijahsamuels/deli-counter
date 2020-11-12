427c3c399dcf5ca27cb2f63ac43dd5f7e30c890a
elijahsamuels <elijahsamuels@gmail.com>
2020-11-11 23:01:41 -0700

    commit tree 2d1ee0c3deebf762452b044da8d6a2781a1481f5

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,58 @@
-def line(peopleInLine)
-    if peopleInLine <= 0
+def line(katz_deli)
+
+    if katz_deli.size < 1
         puts "The line is currently empty."
     else
-        puts "take a number!"
+        lineCurrently = ["The line is currently:"]
+        katz_deli.each.with_index(1) do |name, index|
+            lineCurrently.push(" #{index}. #{name}")
+        end
+
+        puts lineCurrently.join
+    
+    end  
+    
 end
-line(peopleInLine)
+
+
+
+
+def take_a_number(katz_deli, name)
+    
+    if katz_deli.empty?
+        katz_deli.push(name)
+        puts "Welcome, #{name}. You are number 1 in line."
+    
+    else
+        katz_deli.push(name)
+        puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    end
+
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli.first}."
+        katz_deli.shift   
+end
+
+end
+
+
+
+# 
+
+
+#     if take_a_number.size = 1
+#         puts 
+
+#     elsif
+#         lineCurrently = ["The line is currently:"]
+
+
+#     else
+#         katz_deli.size < 1
+#             puts "there is nobody in line"
+#     end
